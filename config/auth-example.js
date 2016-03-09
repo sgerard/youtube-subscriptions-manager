72ab0f4d2f7f6a50fbd6d36f48557fd71b428d63
Sylvain GERARD <sylv.gerard@gmail.com>
2016-03-09 22:31:04 +0100

    Add example files for 'config/auth.js' and 'views/ga.jade'

---
+++ config/auth-example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth': {
+        'clientID': '<your-client-id>.apps.googleusercontent.com',
+        'clientSecret': '<your-client-secret>',
+        'callbackURL': 'https://<your-server-url>/services/youtube/api/users/authcallback'
+    }
+}
